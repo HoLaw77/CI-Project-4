c91437669f383a1f3454c07304b6854a2085746b
HoLaw77 <>
2023-08-01 15:58:02 +0000

    Delete wrong import in views to admin

--- admin/admin.py
+++ admin/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from .models import RAMEN
 # Register your models here.
--- admin/views.py
+++ admin/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-from .models import Ramen
+
 # Create your views here.
